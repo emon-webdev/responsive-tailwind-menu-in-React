312e76dc112f5700067e212bec38e259d400bbcb
emon-webdev <emon.webdev@gmail.com>
2022-11-11 09:57:10 +0600

    menu added

---
+++ index.js
@@ -0,0 +1,63 @@
+import React, { useContext, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from "../../assets/img/travel-logo.png";
+import { AuthContext } from '../../Contexts/AuthProvider';
+
+
+const Header = () => {
+    const [isActive, setIsActive] = useState(false);
+    const navigate = useNavigate();
+
+    const handleHumbagerMenu = event => {
+        setIsActive(current => !current);
+    }
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() => {
+            navigate('/login')
+        })
+        .catch(error => console.error(error))
+    } 
+
+    return (
+        <div className="header">
+            <div className="container mx-auto px-4">
+                <nav className="navbar">
+                    <Link className="navbar-brand" to="/"><img src={logo} alt="Travel" /></Link>
+                    <button className="navbar-toggler" type="button" onClick={handleHumbagerMenu}>
+                        <FaBars className='text-[#0f6191] border-[#0f6191]'></FaBars>
+                    </button>
+                    <div className ={isActive ? 'navbar-collapsed' : 'navbar-collapse'}>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/">Home</Link></li>
+                        <li><Link className="nav-link" to="/tours">Tours</Link></li>
+                        <li><Link className="nav-link" to="/faqs">FAQ's</Link></li>
+                        {
+                            user?.uid ?
+                            <>
+                            {
+                                user?.uid &&
+                                <>
+                                <li><Link className="nav-link" to="/add-tour">Add Tour</Link></li>
+                                <li><Link className="nav-link" to="/reviews">Reviews</Link></li>
+                                <li><button className="logout-btn" onClick={handleLogOut}><FiLogOut className="mr-2 text-base"></FiLogOut> Sign Out</button></li>
+                                </>
+                            }
+                            
+                            </>
+                            :
+                            <li><Link className="nav-link" to="/login">Login</Link></li>
+                        }
+                    </ul>
+                  </div>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
